8260cec2481f5a99b4792406f223a77cdeccbf65
NN <nn4work@gmail.com>
2017-03-28 16:47:26 +0800

    use b4_action to find_group_and_check_permission

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy ]
-
+  before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -10,10 +10,7 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "no permission"
-    end
+
   end
 
   def new
@@ -31,10 +28,7 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "no permission"
-    end
+
     if @group.update(group_params)
       redirect_to groups_path, notice: "update success"
     else
@@ -43,10 +37,7 @@ class GroupsController < ApplicationController
   end
 
   def destroy
-    @group = Group.find(params[:id])
-    if current_user != @group.user
-      redirect_to root_path, alert: "no permission"
-    end
+    
     @group.destroy
     flash[:alert] = "Group deleted"
     redirect_to groups_path
@@ -54,6 +45,12 @@ class GroupsController < ApplicationController
 
 
 private
+  def find_group_and_check_permission
+    @group = Group.find(prams[:id])
+    if current_user != @group.user
+      redirect_to root_path, alert: "no permission"
+    end
+  end
 
   def group_params
     params.require(:group).permit(:title, :description)
